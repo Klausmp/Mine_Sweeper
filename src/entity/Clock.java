de6f92ead93f5ccce611d13bc2015292ebd9f3a4
Klausmp <cartvdennis@gmail.com>
2019-03-11 22:20:50 +0100

    +Clock Class mit Methoden

---
+++ src/entity/Clock.java
@@ -0,0 +1,26 @@
+package entity;
+
+public class Clock extends Entity{
+
+
+    public Clock(int posX, int posY, int wight, int height) {
+        super(posX, posY, wight, height);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    public void start(){
+
+    }
+
+    public void stop(){
+
+    }
+
+    public void reset(){
+
+    }
+}
