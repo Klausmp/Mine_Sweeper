533bf526841e45c3a8037a05b1d482b102ebbbb7
Klausmp <cartvdennis@gmail.com>
2019-03-16 15:18:15 +0100

    + beim klicken kann nun zwischen tiles und nicht tiles unterschieden werden

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -24,7 +24,7 @@ public abstract class Entity {
         return MouseMotion.getMouseDraggedX() >= Util.getTileDisplayPosX(getPosX(), getWight()) && MouseMotion.getMouseDraggedX() <= Util.getTileDisplayPosX(getPosX(), getWight()) + getWight() && MouseMotion.getMouseDraggedY() >= Util.getTileDisplayPosY(getPosY(), getHeight()) && MouseMotion.getMouseDraggedY() <= Util.getTileDisplayPosY(getPosY(), getHeight()) + getHeight();
     }
 
-    public boolean isLeftKlicked() {
+    public boolean isTileLeftKlicked() {
         if (Mouse.getMouseReleasedLeftX() >= Util.getTileDisplayPosX(getPosX(), getWight()) && Mouse.getMouseReleasedLeftX() <= Util.getTileDisplayPosX(getPosX(), getWight()) + getWight() && Mouse.getMouseReleasedLeftY() >= Util.getTileDisplayPosY(getPosY(), getHeight()) && Mouse.getMouseReleasedLeftY() <= Util.getTileDisplayPosY(getPosY(), getHeight()) + getHeight()) {
             Mouse.resetMouse();
             return true;
@@ -33,8 +33,26 @@ public abstract class Entity {
         }
     }
 
-    public boolean isRightKlicked() {
+    public boolean isTileRightKlicked() {
         if (Mouse.getMouseReleasedRightX() >= Util.getTileDisplayPosX(getPosX(), getWight()) && Mouse.getMouseReleasedRightX() <= Util.getTileDisplayPosX(getPosX(), getWight()) + getWight() && Mouse.getMouseReleasedRightY() >= Util.getTileDisplayPosY(getPosY(), getHeight()) && Mouse.getMouseReleasedRightY() <= Util.getTileDisplayPosY(getPosY(), getHeight()) + getHeight()) {
+            Mouse.resetMouse();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isLeftKlicked() {
+        if (Mouse.getMouseReleasedLeftX() >= getPosX() && Mouse.getMouseReleasedLeftX() <= getPosX() + getWight() && Mouse.getMouseReleasedLeftY() >= getPosY() && Mouse.getMouseReleasedLeftY() <= getPosY() + getHeight()) {
+            Mouse.resetMouse();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isRightKlicked() {
+        if (Mouse.getMouseReleasedRightX() >= getPosX() && Mouse.getMouseReleasedRightX() <= getPosX() + getWight() && Mouse.getMouseReleasedRightY() >= getPosY() && Mouse.getMouseReleasedRightY() <= getPosY() + getHeight()) {
             Mouse.resetMouse();
             return true;
         } else {
